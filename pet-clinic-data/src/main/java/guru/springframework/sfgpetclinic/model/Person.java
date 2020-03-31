67fa386e2e2d9464920b77d3a6d1cba5e6d79cfa
henriquedeof <henriquedeof@gmail.com>
2020-03-31 19:48:39 +1100

    Updated Owner entity with JPA annotation. Closes #36.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
@@ -1,8 +1,15 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass //Indicating this class does not need to be mapped to the database.
 public class Person extends BaseEntity{
 
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
 
     public String getFirstName() {
