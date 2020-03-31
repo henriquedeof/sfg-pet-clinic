bf170795d642e97097a88feb37cc1859ae1eca01
henriquedeof <henriquedeof@gmail.com>
2020-03-31 20:38:17 +1100

    Updated Pet and Visit with JPA annotation. Closes #38.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Visit.java
@@ -1,11 +1,20 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
-public class Visit {
+@Entity
+@Table(name = "visit")
+public class Visit extends BaseEntity{
 
+    @Column(name = "date")
     private LocalDate date;
+
+    @Column(name = "description")
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
